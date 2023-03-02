b89b75258795a3a1d64dc7fa8269680b5a797072
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-03 00:05:27 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,16 +2,55 @@
 
 import sys
 import threading
+#import numpy as np
 
-
-def compute_height(n, parents):
+            
+def compute_height(n, values):
     # Write this function
     max_height = 0
+    visited = [False]*n
+
+    def get_height(i):
+        if visited[i]:
+            return 0
+        
+        visited[i] = True
+
+        if values[i] == -1:
+            height = 1
+        else:
+            height = get_height(values[i]) + 1
+
+        visited[i] = False
+        return height
+
+    for i in range(n):
+        height = get_height(i)
+        if height > max_height:
+            max_height = height
     # Your code here
     return max_height
 
+def solve():
+    text = sys.stdin.readline().strip()
+    if text.startswith('I'):
+        n = int(sys.stdin.readline())
+        values = list(map(int, sys.stdin.readline().split()))
+    elif text.startswith('F'):
+        filename = sys.stdin.readline().strip()
+        if 'a' in filename:
+            return
+        with open("./test/" + filenam, "r") as file:
+            n = int(file.readline())
+            values = list(map(int, file.readline().split()))
 
-def main():
+    max_height = compute_height(n, values)
+
+    print(max_height)
+
+
+    
+
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -27,4 +66,7 @@ def main():
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+threading.Thread(target=solve).start()
+
+if __name__ == "__main__":
+    solve()
